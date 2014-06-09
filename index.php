7b23060c6048180428350ea2e9eb7c5363bde9c6
kentucky <knt92@hotmail.fr>
2014-06-09 05:13:22 +0200

    MAJ -- tentative #1 résolution conflits

--- index.php
+++ index.php
@@ -8,62 +8,62 @@ require_once 'required.php';
 
 if(isset($_SESSION['user']))
 {
-$user = unserialize($_SESSION['user']);
-$userId = $user->getId();
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Nestbox - File explore23r</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    $user = unserialize($_SESSION['user']);
+    $userId = $user->getId();
+    ?>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Nestbox - File explorer</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="shortcut icon" href="content/img/logo/logoNestBox.png">
 
-    <link rel=stylesheet type="text/css" href="content/css/style.css">
-    <link rel=stylesheet type="text/css" href="content/bootstrap/css/bootstrap.css">
-    <link rel=stylesheet type="text/css" href="content/bootstrap/css/bootstrap-theme.css">
-    <link rel="shortcut icon" href="content/img/logo/logoNestBox.png">
+        <link rel=stylesheet type="text/css" href="content/css/style.css">
+        <link rel=stylesheet type="text/css" href="content/bootstrap/css/bootstrap.css">
+        <link rel=stylesheet type="text/css" href="content/bootstrap/css/bootstrap-theme.css">
 
-    <!-- Add fancyBox -->
-    <link rel="stylesheet" href="content/css/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+        <!-- Add fancyBox -->
+        <link rel="stylesheet" href="content/css/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
 
-    <!-- Optionally add helpers - button, thumbnail and/or media -->
-    <link rel="stylesheet" href="content/css/jquery.fancybox-buttons.css?v=1.0.5" type="text/css" media="screen" />
+        <!-- Optionally add helpers - button, thumbnail and/or media -->
+        <link rel="stylesheet" href="content/css/jquery.fancybox-buttons.css?v=1.0.5" type="text/css" media="screen" />
 
-    <link rel="stylesheet" href="content/css/jquery.fancybox-thumbs.css?v=1.0.7" type="text/css" media="screen" />
+        <link rel="stylesheet" href="content/css/jquery.fancybox-thumbs.css?v=1.0.7" type="text/css" media="screen" />
 
-    <script src="content/js/jquery.js"></script>
-    <script src="content/js/navigation.js"></script>
-    <script src="content/js/script.js"></script>
+        <script src="content/js/jquery.js"></script>
+        <script src="content/js/navigation.js"></script>
+        <script src="content/js/script.js"></script>
 
-    <!-- Add mousewheel plugin (this is optional) -->
-    <script type="text/javascript" src="content/js/jquery.mousewheel-3.0.6.pack.js"></script>
+        <!-- Add mousewheel plugin (this is optional) -->
+        <script type="text/javascript" src="content/js/jquery.mousewheel-3.0.6.pack.js"></script>
 
-    <!-- Fichiers js fancybox actions-->
-    <script  src="content/js/fancybox/createFolder.js"></script>
-    <script  src="content/js/fancybox/renameElement.js"></script>
-    <script  src="content/js/fancybox/disableElement.js"></script>
-    <script  src="content/js/fancybox/copyElement.js"></script>
-    <script  src="content/js/fancybox/moveElement.js"></script>
-    <script  src="content/js/fancybox/downloadElement.js"></script>
-    <script  src="content/js/fancybox/uploadElement.js"></script>
-
-
-    <script src="content/js/dropfile.js"></script>
-    <!-- FancyBox-->
-    <script type="text/javascript" src="content/js/jquery.fancybox.pack.js?v=2.1.5"></script>
-    <script type="text/javascript" src="content/js/jquery.fancybox-buttons.js?v=1.0.5"></script>
-    <script type="text/javascript" src="content/js/jquery.fancybox-media.js?v=1.0.6"></script>
-    <script type="text/javascript" src="content/js/jquery.fancybox-thumbs.js?v=1.0.7"></script>
-    <script src="content/bootstrap/js/bootstrap.min.js"></script>
+        <!-- Fichiers js fancybox actions-->
+        <script  src="content/js/fancybox/createFolder.js"></script>
+        <script  src="content/js/fancybox/renameElement.js"></script>
+        <script  src="content/js/fancybox/disableElement.js"></script>
+        <script  src="content/js/fancybox/copyElement.js"></script>
+        <script  src="content/js/fancybox/moveElement.js"></script>
+        <script  src="content/js/fancybox/downloadElement.js"></script>
+        <script  src="content/js/fancybox/uploadElement.js"></script>
 
 
-</head>
+        <script src="content/js/dropfile.js"></script>
+        <!-- FancyBox-->
+        <script type="text/javascript" src="content/js/jquery.fancybox.pack.js?v=2.1.5"></script>
+        <script type="text/javascript" src="content/js/jquery.fancybox-buttons.js?v=1.0.5"></script>
+        <script type="text/javascript" src="content/js/jquery.fancybox-media.js?v=1.0.6"></script>
+        <script type="text/javascript" src="content/js/jquery.fancybox-thumbs.js?v=1.0.7"></script>
+        <script src="content/bootstrap/js/bootstrap.min.js"></script>
 
-<?php
-include 'header/menu.php';
-?>
 
-<div id="contenu">
+    </head>
+
+    <?php
+    include 'header/menu.php';
+    ?>
+
+    <div id="contenu">
     <div id="actions">
         <?php
 
@@ -139,21 +139,20 @@ include 'header/menu.php';
     <!--        et des sous-répertoires -->
     <?php
     echo '<div class="content-arbo">';
-        arborescence($userId,"1","/");    // owner, isOwner, dir (à voir pour le dir pour mettre la base ) + owner = idOwner en fonction de l'user qui se connecte
-
-        if(!isset($_GET['dir']) || $_GET['dir'] == "/")
-            contenu($userId,"1","/");
-        else
-        {
-            contenu($userId,"1",$_GET['dir']);
-        }
-    echo '</div>';
-    }
+    arborescence($userId,"1","/");    // owner, isOwner, dir (à voir pour le dir pour mettre la base ) + owner = idOwner en fonction de l'user qui se connecte
+    if(!isset($_GET['dir']) || $_GET['dir'] == "/")
+        contenu($userId,"1","/");
     else
     {
-        header('Location: /Nestbox/view/login.php');
+        contenu($userId,"1",$_GET['dir']);
     }
-    ?>
+    echo '</div>';
+}
+else
+{
+    header('Location: /Nestbox/view/login.php');
+}
+?>
 
     <!---->
     <!--    <!-- liste des fichiers -->
