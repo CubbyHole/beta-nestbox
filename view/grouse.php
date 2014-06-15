4ca81a3067803b7fd773f7cd5ad605be64de4e71
Harry <harry.bellod@supinfo.com>
2014-06-15 18:41:17 +0200

    grouse

--- view/grouse.php
+++ view/grouse.php
@@ -7,3 +7,47 @@
  */
 
 /* Téléchargement anonyme */
+
+
+header('Content-Type: text/html; charset=utf-8');
+$projectRoot = $_SERVER['DOCUMENT_ROOT'].'/Nestbox';
+require_once $projectRoot.'/required.php';
+
+if(isset($_GET['token']))
+{
+    $elementManager = new ElementPdoManager();
+    $userManager = new UserPdoManager();
+    $refElementManager = new RefElementPdoManager();
+
+    $element = $elementManager->findOne(array('downloadLink' => $_GET['token']));
+    $user = $userManager->findById($element->getOwner());
+    $refElement = $refElementManager->findById($element->getRefElement());
+
+    echo '<div id="elementInformations">
+             <p><label name="description">Element information:</label></p>
+        <ul>
+            <li>Element name : '.$element->getName().'</li>
+            <li>Extension : '.$refElement->getExtension().'</li>
+            <li>Type : '.$refElement->getDescription().'</li>';
+            if(!(preg_match('/^4/', $refElement->getCode())))
+            {
+                if($element->getSize() < 1)
+                    echo '<li>Size : < 1 KB</li>';
+                else
+                    echo '<li>Size : '.$element->getSize().' KB</li>';
+            }
+            echo '<li>Owner : '.$user->getFirstName().' '.$user->getLastName().'</li>
+
+          </div>';
+    ?>
+        <!-- formulaire pour déplacer -->
+        <form id="downloadLinkElement" method="POST">
+            <?php
+            echo '<p><label name="createDownloadLink">Do you want to download this element:</label></p>';
+            echo '<input type="hidden" name="idElement" id="idElement" value="'.$element->getId().'" read-only>';
+            ?>
+    <p style="text-align: center;"><input type="submit" class="btn-success btn" value="Download" name="downloadAnonymousElem" id="downloadAnonymousElem"></p>
+    </form>
+
+<?php
+}
